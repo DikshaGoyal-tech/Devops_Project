21e242a98ff9ebf9c8e835292c38fd3cf2ecef2d
DikshaGoyal-tech <dikshag1912@gmail.com>
2025-10-01 12:44:02 +0530

    Create iam-policies.tf

---
+++ Terraform/iam-policies.tf
@@ -0,0 +1,18 @@
+
+// infra/terraform/policies/external-secrets-read.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:ListSecrets"
+      ],
+      "Resource": "arn:aws:secretsmanager:REGION:ACCOUNT_ID:secret:prod/myapp/*"
+    }
+  ]
+}
+ 
+ 
